389633e8ccd2dd682ae64762646c2ca0c81e4acb
GreenTAM <stambadou@groupeisi.com>
2023-03-05 05:15:41 +0800

    run command added

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var app = express();
 require('dotenv').config()
+var child_process = require('child_process');
 
 app.get('/', function(req, res) {
     const variable = process.env.DB_CLIENT;
-	res.send("Hello World! -- " + variable);
+    const message = {}
+    var erreur = false
+child_process.exec(
+    'npm run start',
+    {},
+    function(error, stdout, stderr) {
+        process.stdout.write(stdout + '\n');
+        process.stderr.write(stderr + '\n');
+        if (error !== null) {
+            message.error = 'erreur \nmessage: ' + stderr
+            console.log(`Erreur: ${erreur}`);
+
+        }
+        else {
+            message.success =  'commande runned on port ' + process.env.PORT + '\nmessage: ' + stdout
+
+            console.log(`Reussi: ${erreur}`);
+
+        }
+    });
+
+    const status = message.error ? 500 : 200
+	res.status(status).send(message);
 });
 
 app.listen(3000);
