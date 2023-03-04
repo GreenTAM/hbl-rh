91de02277f0e97b45e14c65c9da968d1f57fe417
GreenTAM <stambadou@groupeisi.com>
2023-03-05 04:34:38 +0800

    dot env added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 app.get('/', function(req, res) {
-    const variable = process.env.ADMIN_EMAIL;
+    const variable = process.env.DB_CLIENT;
 	res.send("Hello World! -- " + variable);
 });
 
